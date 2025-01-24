c8484d995109ad665ac8d73d3049732064d00211
Juanes <juanespachon1203@gmail.com>
2025-01-24 15:55:16 -0500

    feat(header): Add header Component

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,36 @@
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  // scroll to section logic
+
+  scrollToSection(sectionId: string): void {
+      
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const offsetTop = element.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: offsetTop - 100,
+        behavior: 'smooth',
+      });
+    }
+  
+  }
+
+  // open Mobile menu logic
+
+  isMenuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.isMenuOpen.update((value) => !value);
+  }
+
+}

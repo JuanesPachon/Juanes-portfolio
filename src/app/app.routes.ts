6c863eb81573b8cb20a443ae05d742966cf8b9be
Juanes <juanespachon1203@gmail.com>
2025-01-27 20:38:48 -0500

    feat(ui): add route transition animations and update favicon and page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectPageComponent } from './pages/project-page/project-page.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'project/:id', component: ProjectPageComponent },
+    { path: '', component: HomeComponent, title: 'Juanes Pachón 🔗 - Home'},
+    { path: 'project/:id', component: ProjectPageComponent, title: 'Juanes Pachón 🔗 - Projects'},
 ];
